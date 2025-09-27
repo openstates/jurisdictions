479d92aeac5cf201ac31a11b7a23b5ba055d1984
MightyLoud <creytonloud2@gmail.com>
2025-09-27 14:40:03 -0600

    addjuristiciontestobjects

---
+++ src/jurisdictions_sample.py
@@ -0,0 +1,22 @@
+if __name__ == "__main__":
+
+    harbor_hills = Jurisdiction(
+        id="ocd-jurisdiction/country:us/state:oh/place:harbor_hills",
+        name="Harbor Hills CDP",
+        url="https://lickingcounty.gov/",
+        classification=ClassificationEnum.GOVERNMENT,
+    )
+
+    parma = Jurisdiction(
+        id="ocd-jurisdiction/country:us/state:oh/place:parma",
+        name="Parma City",
+        url="https://www.cityofparma-oh.gov/",
+        classification=ClassificationEnum.GOVERNMENT,
+    )
+
+    valleyview = Jurisdiction(
+        id="ocd-jurisdiction/country:us/state:oh/place:valleyview",
+        name="Valleyview Village",
+        url="https://www.valleyviewohio.org/",
+        classification=ClassificationEnum.GOVERNMENT,
+    )
