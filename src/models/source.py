234d2a5820ca6a75dadd19a62ba711489837c6de
__token__ <kas@bluesquad.co>
2025-09-27 13:11:46 -0400

    Source model

--- src/models/source.py
+++ src/models/source.py
@@ -1,14 +1,13 @@
 from enum import Enum
 from pydantic import BaseModel, Field
-from typing import Any
-from urllib import TK
+from pydantic import AnyHttpUrl, FtpUrl, FileUrl
+from typing import Union
 
 """
 LSADS - No legal entity
 CCD 22,
 CDP 57,
 Sub-District 24, unlikely
-
 """
 
 class SourceType(str, Enum):
@@ -20,7 +19,7 @@ class SourceType(str, Enum):
 class SourceObj(BaseModel):
     """A source object. Used for determining the valid-thru and veracity of the data collected."""
     source_type: SourceType = Field(default=SourceType.HUMAN, description="The method used to collect the data.")
-    source_url: dict[str, URL] = Field(description = "The source name and url. If AI generated, the source url identified by the AI agent.")
+    source_url: dict[str, Union[AnyHttpUrl, FtpUrl, FileUrl]] = Field(description = "The source name and url. If AI generated, the source url identified by the AI agent.")
     source_description: str | None = Field(description="A brief descriptoin of how the data was sourced. I.e. ArcGIS library provided by arcgismapper.com")
     field: list[str] = Field(description = "An array of fields that this SourceObj refers to, can be more than one. Validated against the available fields in the model.")
 
