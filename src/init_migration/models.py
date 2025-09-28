68f5ac7433315246cbae328a9b0e5884bf0f4604
__token__ <kas@bluesquad.co>
2025-09-28 14:55:26 -0400

    request, resp models for ingest pipelines

---
+++ src/init_migration/models.py
@@ -0,0 +1,23 @@
+from pydantic import Field, BaseModel
+from uuid import UUID
+from pathlib import Path
+from src.models.ocdid import OCDidParsed
+from typing import Any
+
+
+class OCDidIngestResp(BaseModel):
+    uuid:UUID
+    filepath: Path
+    parsed_ocdid: OCDidParsed
+    raw_record: dict[str, Any]
+
+
+# This request object allows the caller to determine which parts of the data to
+# focus on enable the pipeline to be run to fill different aspects of the .yaml
+# file.
+class DivGeneratorReq(BaseModel):
+    data: OCDidIngestResp
+    base_object: bool
+    ai_url: bool # Wether or not to populate url data w/ai scraper
+    geo_req: bool # Whether or not to populate geo request data
+    population_req: bool # Wether or not to populate with Census population API call.
